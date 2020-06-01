8c6a91eca65103ec906fb962ab1f0b6bf547c62a
Andrei Zuica <zuica.andrei@gmail.com>
2020-06-01 20:36:48 +0300

    Fixed tests before release

--- src/test/java/io/jenkins/plugins/wattspeed/WattspeedBuilderTest.java
+++ src/test/java/io/jenkins/plugins/wattspeed/WattspeedBuilderTest.java
@@ -11,14 +11,14 @@ public class WattspeedBuilderTest
   @Rule
   public JenkinsRule jenkins = new JenkinsRule();
 
-  final Secret token = Secret.fromString("ZBOknBYGBDOS8hz7kSjH/R1lPEnUx3ztph2kyFN4A9AfM3i1aMPKKJPRgPawS+aB");
+  final String token = "ZBOknBYGBDOS8hz7kSjH/R1lPEnUx3ztph2kyFN4A9AfM3i1aMPKKJPRgPawS+aB";
   final int project_id = 1;
 
   @Test
   public void testBuild() throws Exception
   {
     FreeStyleProject project = jenkins.createFreeStyleProject();
-    WattspeedBuilder builder = new WattspeedBuilder(token, project_id);
+    WattspeedBuilder builder = new WattspeedBuilder(Secret.fromString(token), project_id);
     project.getBuildersList().add(builder);
 
     jenkins.buildAndAssertSuccess(project);
